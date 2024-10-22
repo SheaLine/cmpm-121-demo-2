439eafdcc8ceb2c6a24b5e5183cb4a0f8d27d446
SheaLine <sline@ucsc.edu>
2024-10-22 21:20:51 +0000

    Step 3 complete

--- src/main.ts
+++ src/main.ts
@@ -19,23 +19,21 @@ document.body.append(clearButton);
 const ctx = canvas.getContext("2d")!;
 
 const cursor = { active: false, x: 0, y: 0 };
+const lines: { x: number; y: number }[][] = [[]];
 
 canvas.addEventListener("mousedown", (e) => {
     cursor.active = true;
     cursor.x = e.offsetX;
     cursor.y = e.offsetY;
+    lines.push([{ x: cursor.x, y: cursor.y }]);
+    canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
 canvas.addEventListener("mousemove", (e) => {
-    ctx.lineWidth = 3;
-    ctx.lineCap = "round";
-    ctx.strokeStyle = "black";
-
     if (cursor.active) {
-        ctx.beginPath();
-        ctx.moveTo(cursor.x, cursor.y);
-        ctx.lineTo(e.offsetX, e.offsetY);
-        ctx.stroke();
+        const newPoint = { x: e.offsetX, y: e.offsetY };
+        lines[lines.length - 1].push(newPoint);
+        canvas.dispatchEvent(new Event("drawing-changed"));
         cursor.x = e.offsetX;
         cursor.y = e.offsetY;
     }
@@ -46,9 +44,25 @@ canvas.addEventListener("mouseup", () => {
 });
 
 clearButton.addEventListener("click", () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    lines.length = 0;
+    lines.push([]);
+    canvas.dispatchEvent(new Event("drawing-changed"));
 });
 
-clearButton.addEventListener("click", () => {
+canvas.addEventListener("drawing-changed", () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+
+    lines.forEach((line) => {
+        if (line.length > 1) {
+            ctx.beginPath();
+            ctx.moveTo(line[0].x, line[0].y);
+            for (let i = 1; i < line.length; i++) {
+                ctx.lineTo(line[i].x, line[i].y);
+            }
+            ctx.stroke();
+        }
+    });
 });
