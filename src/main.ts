d946e76e9f0fbbbf896bed3d264bd5d00240040d
SheaLine <sline@ucsc.edu>
2024-10-22 20:31:54 +0000

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -8,5 +8,47 @@ title.textContent = APP_NAME;
 app.appendChild(title);
 
 const canvas = document.createElement("canvas");
-canvas.id = "appCanvas";
+canvas.width = 256;
+canvas.height = 256;
 app.appendChild(canvas);
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "clear";
+document.body.append(clearButton);
+
+const ctx = canvas.getContext("2d")!;
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+    cursor.active = true;
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+    ctx.lineWidth = 3;
+    ctx.lineCap = "round";
+    ctx.strokeStyle = "black";
+
+    if (cursor.active) {
+        ctx.beginPath();
+        ctx.moveTo(cursor.x, cursor.y);
+        ctx.lineTo(e.offsetX, e.offsetY);
+        ctx.stroke();
+        cursor.x = e.offsetX;
+        cursor.y = e.offsetY;
+    }
+});
+
+canvas.addEventListener("mouseup", () => {
+    cursor.active = false;
+});
+
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+clearButton.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
